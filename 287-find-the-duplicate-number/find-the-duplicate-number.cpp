e987f425345fbca962566d648068acef615f1402
Shivam thakur <162620095+Theonlyshivam@users.noreply.github.com>
2026-07-26 09:59:08 +0530

    Time: 4 ms (60.60%) | Memory: 65.1 MB (42.11%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        //BRUTE FORCE -> USING UNORDERD SET
+        // use slow - fast pointer , imagine an array as linklist and the move slow by 1 and fast by 2 and whenever slow == fast then , again start slow from 0th index and now move slow by 1 and fast by when now whenever slow == fast then that is the element which is reapeating.
 
-        unordered_set<int>s;
 
-        for(int val:nums){
-            if(s.find(val)!=s.end()){
-                return val;
-            }
-            s.insert(val);
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do{
+            slow = nums[slow]; //+1
+            fast = nums[nums[fast]]; //+2;
+        } while(slow!=fast);
+
+        slow =  nums[0];
+        while(slow!=fast){
+            slow=nums[slow]; //+1
+            fast = nums[fast];//1;
+
         }
-        return -1;
+        return slow;
     }
 };
