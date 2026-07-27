1e253734baad89fba2cde7388e6e651fc7d2f8c1
Shivam thakur <162620095+Theonlyshivam@users.noreply.github.com>
2026-07-27 10:22:17 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (61.63%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+        // i have to find the prefix product first means where i am standing i have to find the product of all elements just before that element where  i am standing  ,, and i same way i have to find the suffix means i need to find the product of all elemnts just after where i am standing and then just product prefix and suffix->ans;
+        // NOW FOR SPACE OPTIMIZATION WE CAN DIRECTLY STROE THE PREFIX PRODUCT INTO MY ANS ARRAY AND FOR SUFFIX I CAN TAKE ONE SUFFIX VARIABLE INITAILIZE WITH 1 ANS JUST FIND THE SUFFIX PRODUCT AND DIRECTLY STROE AT ANS[I];
         int n = nums.size();
         vector<int>ans(n,1);
-         vector<int>prefix(n,1);
-          vector<int>suffix(n,1);
+         
 
           //prefix
 
           for(int i=1;i<n;i++){
-            prefix[i] = prefix[i-1] * nums[i-1];
+            ans[i] = ans[i-1]*nums[i-1];
           }
           
           //suffix
+          int suffix = 1;
           for(int i=n-2;i>=0;i--){
-            suffix[i] = suffix[i+1] * nums[i+1];
+            suffix *= nums[i+1];
+            ans[i] *= suffix;
           }
 
-          for(int i=0;i<n;i++){
-            ans[i]= prefix[i]*suffix[i];
-          }
+          
           return ans;
 
     }
